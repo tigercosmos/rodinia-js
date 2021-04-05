afc1f1c4efabb40014ce602b7bda1605a54ef3c5
tigercosmos <phy.tiger@gmail.com>
2021-04-05 16:10:15 +0800

    bfs serial

---
+++ js/bfs/main_serial.js
@@ -0,0 +1,160 @@
+
+//Structure to hold a node information
+var Node = {
+	starting: null,
+	no_of_edges: null
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// Main Program
+////////////////////////////////////////////////////////////////////////////////
+async function main(argc) {
+	const result = await BFSGraph(argc);
+    await checkResult(result);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//Apply BFS on a Graph using CUDA
+////////////////////////////////////////////////////////////////////////////////
+async function BFSGraph(argc) {
+	var no_of_nodes = 0;
+	var edge_list_size = 0;
+	var input_f;
+	var num_omp_threads;
+
+	// num_omp_threads = argc.num_omp_threads;
+	// input_f = argc.input_f;
+
+	console.log("Reading File\n");
+	//Read in Graph from a file
+	var data = await fetch("data.txt");
+
+    var text = await data.text();
+
+	data = text.toString().split("\n");
+
+	var source = 0;
+
+	var line_counter = 0;
+	no_of_nodes = Number(data[line_counter++]);
+
+	// allocate host memory
+	var h_graph_nodes = []; 
+	var h_graph_mask_buf = new SharedArrayBuffer(no_of_nodes * Int8Array.BYTES_PER_ELEMENT)
+	var h_updating_graph_mask_buf = new SharedArrayBuffer(no_of_nodes * Int8Array.BYTES_PER_ELEMENT)
+	var h_graph_visited_buf = new SharedArrayBuffer(no_of_nodes * Int8Array.BYTES_PER_ELEMENT)
+
+	var h_graph_mask = new Int8Array(h_graph_mask_buf);
+	var h_updating_graph_mask = new Int8Array(h_updating_graph_mask_buf);
+	var h_graph_visited = new Int8Array(h_graph_visited_buf);
+
+	var start, edgeno;
+	// initalize the memory
+	for (var i = 0; i < no_of_nodes; i++) {
+		var tmp_data = data[line_counter++].split(" ");
+		start = Number(tmp_data[0]);
+		edgeno = Number(tmp_data[1]);
+
+		h_graph_nodes[i] = {
+			starting: start,
+			no_of_edges: edgeno
+		};
+
+		h_graph_mask[i] = 0;
+		h_updating_graph_mask[i] = 0;
+		h_graph_visited[i] = 0;
+	}
+
+
+	//read the source node from the file
+	source = Number(data[line_counter++]);
+	if (source != 0) {
+		console.error("Input data wrong");
+	}
+
+	//set the source node as 1 in the mask
+	h_graph_mask[source] = 1;
+	h_graph_visited[source] = 1;
+
+	edge_list_size = Number(data[line_counter++]);
+
+	console.log("edge_list_size", edge_list_size)
+
+	var _id, cost;
+	var h_graph_edges = []
+	for (var i = 0; i < edge_list_size; i++) {
+		var tmp_data = data[line_counter++].split(" ");
+		_id = Number(tmp_data[0]);
+		cost = Number(tmp_data[1]); // no used
+		h_graph_edges[i] = _id;
+	}
+
+	// allocate mem for the result on host side
+	var h_cost_buf = new SharedArrayBuffer(no_of_nodes * Int32Array.BYTES_PER_ELEMENT)
+	var h_cost = new Int32Array(h_cost_buf)
+	for (var i = 0; i < no_of_nodes; i++)
+		h_cost[i] = -1;
+	h_cost[source] = 0;
+
+	console.log("Start traversing the tree");
+
+	var k = 0;
+
+	var start_time = Date.now();
+
+	var stop;
+	do {
+		//if no thread changes this value then the loop stops
+		stop = 0;
+
+		// #pragma omp parallel for 
+		for (var tid = 0; tid < no_of_nodes; tid++) {
+			if (h_graph_mask[tid] == 1) {
+				h_graph_mask[tid] = 0;
+				for (var i = h_graph_nodes[tid].starting; i < (h_graph_nodes[tid].no_of_edges + h_graph_nodes[tid].starting); i++) {
+					var id = h_graph_edges[i];
+					if (!h_graph_visited[id]) {
+						h_cost[id] = h_cost[tid] + 1;
+						h_updating_graph_mask[id] = 1;
+					}
+				}
+			}
+		}
+
+		// #pragma omp parallel for
+		for (var tid = 0; tid < no_of_nodes; tid++) {
+			if (h_updating_graph_mask[tid] == 1) {
+				h_graph_mask[tid] = 1;
+				h_graph_visited[tid] = 1;
+				stop = 1;
+				h_updating_graph_mask[tid] = 0;
+			}
+		}
+		k++;
+	}
+	while (stop);
+
+	var end_time = Date.now();
+	console.log("Compute time:", (end_time - start_time));
+
+	//Store the result into a file
+	var result = [];
+	for (var i = 0; i < no_of_nodes; i++)
+		result.push(`${i}) cost:${h_cost[i]}`)
+
+	console.log("Result stored in result.txt");
+
+    return result;
+}
+
+async function checkResult(result) {
+    var data = await fetch("answer.txt");
+    var answer = (await data.text()).split('\n');
+    for(var i = 0; i < result.length; i++) {
+        if(result[i] != answer[i]) {
+            console.error(`Line ${i}: ${result[i]} != ${answer[i]}`);
+        }
+    }
+}
+
+main();
